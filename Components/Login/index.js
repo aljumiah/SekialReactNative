fed7f3024f81a4ecdc2678462185a6d676c6c36e
riri-jp <alahmary.mashari@gmail.com>
2019-04-16 20:57:35 +0300

    login

---
+++ Components/Login/index.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import * as actionCreators from "../../store/actions";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert
+} from "react-native";
+
+class Login extends Component {
+  componentDidMount = () => {
+    this.props.checkForToken();
+  };
+
+  state = {
+    username: "",
+    password: ""
+  };
+
+  render() {
+    if (this.props.user) {
+      //   this.props.navigation.navigate("");
+    }
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Image
+            style={styles.inputIcon}
+            source={{
+              uri: "https://img.icons8.com/ios/80/000000/phone-filled.png"
+            }}
+          />
+          <TextInput
+            style={styles.inputs}
+            placeholder="Phone Number"
+            keyboardType="username"
+            onChangeText={username => this.setState({ username })}
+            value={this.state.username}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Image
+            style={styles.inputIcon}
+            source={{
+              uri: "https://img.icons8.com/ios/80/000000/lock-2-filled.png"
+            }}
+          />
+          <TextInput
+            style={styles.inputs}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={password => this.setState({ password })}
+          />
+        </View>
+
+        <TouchableHighlight
+          style={[styles.buttonContainer, styles.loginButton]}
+          onPress={() => this.props.login(this.state)}
+        >
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          style={styles.buttonContainer}
+          onPress={() => this.props.signup(this.state)}
+        >
+          <Text>Register</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#DCDCDC"
+  },
+  inputContainer: {
+    borderBottomColor: "#F5FCFF",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: "#FFFFFF",
+    flex: 1
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec"
+  },
+  loginText: {
+    color: "white"
+  }
+});
+const mapStateToProps = state => ({
+  user: state.authReducer.user
+});
+const mapDispatchToProps = dispatch => ({
+  login: userData => dispatch(actionCreators.login(userData)),
+  signup: userData => dispatch(actionCreators.signup(userData)),
+  checkForToken: () => dispatch(actionCreators.checkForExpiredToken())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
